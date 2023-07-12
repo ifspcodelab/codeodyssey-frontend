f479bbe9cbeaab89533bdf37e414869725ed527a
Domingos Latorre <domingos.latorre@gmail.com>
2023-07-12 08:18:57 -0300

    feat: setup react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
+import {Outlet} from "react-router-dom";
+
 function App() {
-  return (
-    <>
-      <h1>App</h1>
-    </>
-  )
+    return <Outlet/>
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Home from "./pages/Home";
+import TermsOfUse from "./pages/TermsOfUse";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        children: [
+            {
+                path: "",
+                element: <Home/>
+            },
+            {
+                path: "terms-of-use",
+                element: <TermsOfUse/>
+            },
+            {
+                path: "privacy-policy",
+                element: <PrivacyPolicy/>
+            },
+        ],
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>,
 )
