3052e646a88501c2db96ff1ef31a7feba7d3e891
vinicius <viniciusarashiro0@gmail.com>
2023-07-25 18:07:33 -0300

    feat: slug validation

--- src/pages/CreateCourse/schema.ts
+++ src/pages/CreateCourse/schema.ts
@@ -2,7 +2,10 @@ import Yup from "../../config/yup.ts";
 
 export const schema = Yup.object({
     name: Yup.string().required().min(1).max(255),
-    slug: Yup.string().required().min(1).max(255),
+    slug: Yup.string().required().min(1).max(255).matches(
+        /^[a-zA-Z0-9]+$/,
+        "This field cannot contain white space and special character"
+      ),
     startDate: Yup.date().required(),
     endDate: Yup.date().required().min(Yup.ref('startDate'))
 }).required()
