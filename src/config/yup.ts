9c95a7e8727af8824c5e1195f366e28a035c57e7
rafaelspa <rsdepaula67@gmail.com>
2023-07-31 08:34:00 -0300

    feat: add yup locale objects and its translations

--- src/config/yup.ts
+++ src/config/yup.ts
@@ -10,16 +10,46 @@ const locale = {
         oneOf: i18n.t("yup.mixed_one_of"),
         max: i18n.t("yup.mixed_max"),
         min: i18n.t("yup.mixed_min"),
+        defined: i18n.t("yup.mixed_defined"),
+        notNull: i18n.t("yup.mixed_not_null"),
     },
     string: {
         email: i18n.t("yup.string_email"),
         max: i18n.t("yup.string_max"),
         min: i18n.t("yup.string_min"),
+        matches: i18n.t("yup.string_matches"),
+        length: i18n.t("yup.string_length"),
+        url: i18n.t("yup.string_url"),
+        uuid: i18n.t("yup.string_uuid"),
+        trim: i18n.t("yup.string_trim"),
+        lowercase: i18n.t("yup.string_lowercase"),
+        uppercase: i18n.t("yup.string_uppercase"),
     },
     number: {
         max: i18n.t("yup.number_max"),
         min: i18n.t("yup.number_min"),
+        lessThan: i18n.t("yup.number_less_than"),
+        moreThan: i18n.t("yup.number_more_than"),
+        notEqual: i18n.t("yup.number_not_equal"),
+        positive: i18n.t("yup.number_positive"),
+        negative: i18n.t("yup.number_negative"),
+        integer: i18n.t("yup.number_integer"),
     },
+    date: {
+        min: i18n.t("yup.date_min"),
+        max: i18n.t("yup.date_max"),
+    },
+    boolean: {
+        isValue: i18n.t("yup.boolean_is_value")
+    },
+    object: {
+        noUnknown: i18n.t("yup.object_no_unknown")
+    },
+    array: {
+        min: i18n.t("yup.array_min"),
+        max: i18n.t("yup.array_max"),
+        length: i18n.t("yup.array_length"),
+    }
 };
 
 Yup.setLocale(locale);
