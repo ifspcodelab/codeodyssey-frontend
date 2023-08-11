9adfb386e80c9d20107df74f8fb2647044df8715
rafaelspa <rsdepaula67@gmail.com>
2023-08-11 16:10:35 -0300

    test: refactor all tests

--- src/pages/Login/login.test.tsx
+++ src/pages/Login/login.test.tsx
@@ -5,33 +5,38 @@ import {BrowserRouter} from "react-router-dom";
 import "@testing-library/jest-dom";
 import userEvent from '@testing-library/user-event'
 
+function renderLogin() {
+    return render(
+        <BrowserRouter>
+            <Login/>
+        </BrowserRouter>
+    );
+}
+
+const createTestVariables = () => {
+    const {getByText, getByRole, getByLabelText} = renderLogin()
+
+    const inputEmail = getByLabelText('Email') as HTMLInputElement;
+    const inputPassword = getByLabelText('Password') as HTMLInputElement;
+    const loginButton = getByRole('button', { name: 'Login' }) as HTMLButtonElement;
+
+    return { getByRole, getByLabelText, inputEmail, inputPassword, loginButton, getByText }
+}
 
 describe("Login", () => {
     test("Should be able to see all page elements on the screen", () => {
-        const { getByRole, getByLabelText } = render(
-            <BrowserRouter>
-                <Login/>
-            </BrowserRouter>
-        );
+        const { getByRole, getByLabelText } = renderLogin();
 
         expect(getByRole("heading", { name: "Login"})).toBeInTheDocument();
-        expect(getByRole("heading", { name: "Log in or Register"})).toBeInTheDocument();
+        expect(getByRole("heading", { name: "Sign in or Register"})).toBeInTheDocument();
         expect(getByRole("textbox", { name: "Email"})).toBeInTheDocument();
         expect(getByLabelText("Password")).toBeInTheDocument();
-        expect(getByRole("button", { name: "Log In!"})).toBeInTheDocument();
+        expect(getByRole("button", { name: "Login"})).toBeInTheDocument();
         expect(getByRole("button", { name: "Register"})).toBeInTheDocument();
     })
 
     test("Should send required email error message", async () => {
-        const {getByText, getByLabelText} = render(
-            <BrowserRouter>
-                <Login/>
-            </BrowserRouter>
-        );
-
-        const inputEmail = getByLabelText('Email') as HTMLInputElement;
-        const inputPassword = getByLabelText('Password') as HTMLInputElement;
-        const submitButton = getByText('Log In!') as HTMLButtonElement;
+        const {getByText, inputEmail, inputPassword, loginButton} = createTestVariables();
 
         fireEvent.change(inputEmail, {target: {value: ''}});
         fireEvent.change(inputPassword, {target: {value: 'Password@01'}});
@@ -39,21 +44,13 @@ describe("Login", () => {
         expect(inputPassword.value).toEqual('Password@01');
 
         await waitFor(() => {
-            void userEvent.click(submitButton);
+            void userEvent.click(loginButton);
             expect(getByText(/email is a required field/i)).toBeInTheDocument();
         })
     })
 
     test("Should send valid email error message", async () => {
-        const {getByText, getByLabelText} = render(
-            <BrowserRouter>
-                <Login/>
-            </BrowserRouter>
-        );
-
-        const inputEmail = getByLabelText('Email') as HTMLInputElement;
-        const inputPassword = getByLabelText('Password') as HTMLInputElement;
-        const submitButton = getByText('Log In!') as HTMLButtonElement;
+        const {getByText, inputEmail, inputPassword, loginButton} = createTestVariables();
 
         fireEvent.change(inputEmail, {target: {value: 'johndoeemail.com'}});
         fireEvent.change(inputPassword, {target: {value: 'Password@01'}});
@@ -61,21 +58,13 @@ describe("Login", () => {
         expect(inputPassword.value).toEqual('Password@01');
         
         await waitFor(() => {
-            void userEvent.click(submitButton);
+            void userEvent.click(loginButton);
             expect(getByText(/email must be a valid email/i)).toBeInTheDocument();
         })
     })
 
     test("Should send uppercase letter error message", async () => {
-        const {getByText, getByLabelText} = render(
-            <BrowserRouter>
-                <Login/>
-            </BrowserRouter>
-        );
-
-        const inputEmail = getByLabelText('Email') as HTMLInputElement;
-        const inputPassword = getByLabelText('Password') as HTMLInputElement;
-        const submitButton = getByText('Log In!') as HTMLButtonElement;
+        const {getByText, inputEmail, inputPassword, loginButton} = createTestVariables();
 
         fireEvent.change(inputEmail, {target: {value: 'johndoe@email.com'}});
         fireEvent.change(inputPassword, {target: {value: 'password@01'}});
@@ -83,21 +72,13 @@ describe("Login", () => {
         expect(inputEmail.value).toEqual('johndoe@email.com');
 
         await waitFor(() => {
-            void userEvent.click(submitButton);
+            void userEvent.click(loginButton);
             expect(getByText(/Password must have at least one uppercase letter/i)).toBeInTheDocument();
         })
     })
 
     test("Should send uppercase letter error message", async () => {
-        const {getByText, getByLabelText} = render(
-            <BrowserRouter>
-                <Login/>
-            </BrowserRouter>
-        );
-
-        const inputEmail = getByLabelText('Email') as HTMLInputElement;
-        const inputPassword = getByLabelText('Password') as HTMLInputElement;
-        const submitButton = getByText('Log In!') as HTMLButtonElement;
+        const {getByText, inputEmail, inputPassword, loginButton} = createTestVariables();
 
         fireEvent.change(inputEmail, {target: {value: 'johndoe@email.com'}});
         fireEvent.change(inputPassword, {target: {value: 'PASSWORD@01'}});
@@ -105,21 +86,13 @@ describe("Login", () => {
         expect(inputEmail.value).toEqual('johndoe@email.com');
 
         await waitFor(() => {
-            void userEvent.click(submitButton);
+            void userEvent.click(loginButton);
             expect(getByText(/Password must have at least one lowercase letter/i)).toBeInTheDocument();
         })
     })
 
     test("Should send special character error message", async () => {
-        const {getByText, getByLabelText} = render(
-            <BrowserRouter>
-                <Login/>
-            </BrowserRouter>
-        );
-
-        const inputEmail = getByLabelText('Email') as HTMLInputElement;
-        const inputPassword = getByLabelText('Password') as HTMLInputElement;
-        const submitButton = getByText('Log In!') as HTMLButtonElement;
+        const {getByText, inputEmail, inputPassword, loginButton} = createTestVariables();
 
         fireEvent.change(inputEmail, {target: {value: 'johndoe@email.com'}});
         fireEvent.change(inputPassword, {target: {value: 'Passworda01'}});
@@ -127,43 +100,14 @@ describe("Login", () => {
         expect(inputEmail.value).toEqual('johndoe@email.com');
 
         await waitFor(() => {
-            void userEvent.click(submitButton);
-            expect(getByText(/Password must have at least one special character/i)).toBeInTheDocument();
-        })
-    })
-
-    test("Should send special character error message", async () => {
-        const {getByText, getByLabelText} = render(
-            <BrowserRouter>
-                <Login/>
-            </BrowserRouter>
-        );
-
-        const inputEmail = getByLabelText('Email') as HTMLInputElement;
-        const inputPassword = getByLabelText('Password') as HTMLInputElement;
-        const submitButton = getByText('Log In!') as HTMLButtonElement;
-
-        fireEvent.change(inputEmail, {target: {value: 'johndoe@email.com'}});
-        fireEvent.change(inputPassword, {target: {value: 'Passworda01'}});
-
-        expect(inputEmail.value).toEqual('johndoe@email.com');
-
-        await waitFor(() => {
-            void userEvent.click(submitButton);
+            void userEvent.click(loginButton);
             expect(getByText(/Password must have at least one special character/i)).toBeInTheDocument();
         })
     })
 
     test("Should send digits error message", async () => {
-        const {getByText, getByLabelText} = render(
-            <BrowserRouter>
-                <Login/>
-            </BrowserRouter>
-        );
+        const {getByText, inputEmail, inputPassword, loginButton} = createTestVariables();
 
-        const inputEmail = getByLabelText('Email') as HTMLInputElement;
-        const inputPassword = getByLabelText('Password') as HTMLInputElement;
-        const submitButton = getByText('Log In!') as HTMLButtonElement;
 
         fireEvent.change(inputEmail, {target: {value: 'johndoe@email.com'}});
         fireEvent.change(inputPassword, {target: {value: 'Password@ab'}});
@@ -171,21 +115,13 @@ describe("Login", () => {
         expect(inputEmail.value).toEqual('johndoe@email.com');
 
         await waitFor(() => {
-            void userEvent.click(submitButton);
+            void userEvent.click(loginButton);
             expect(getByText(/Password must have at least one digit/i)).toBeInTheDocument();
         })
     })
 
     test("Should send required email error message", async () => {
-        const {getByText, getByLabelText} = render(
-            <BrowserRouter>
-                <Login/>
-            </BrowserRouter>
-        );
-
-        const inputEmail = getByLabelText('Email') as HTMLInputElement;
-        const inputPassword = getByLabelText('Password') as HTMLInputElement;
-        const submitButton = getByText('Log In!') as HTMLButtonElement;
+        const {getByText, inputEmail, inputPassword, loginButton} = createTestVariables();
 
         fireEvent.change(inputEmail, {target: {value: 'johndoe@email.com'}});
         fireEvent.change(inputPassword, {target: {value: ''}});
@@ -193,21 +129,13 @@ describe("Login", () => {
         expect(inputEmail.value).toEqual('johndoe@email.com');
 
         await waitFor(() => {
-            void userEvent.click(submitButton);
+            void userEvent.click(loginButton);
             expect(getByText(/password is a required field/i)).toBeInTheDocument();
         })
     })
 
     test("Should send minimum characters length error message", async () => {
-        const {getByText, getByLabelText} = render(
-            <BrowserRouter>
-                <Login/>
-            </BrowserRouter>
-        );
-
-        const inputEmail = getByLabelText('Email') as HTMLInputElement;
-        const inputPassword = getByLabelText('Password') as HTMLInputElement;
-        const submitButton = getByText('Log In!') as HTMLButtonElement;
+        const {getByText, inputEmail, inputPassword, loginButton} = createTestVariables();
 
         fireEvent.change(inputEmail, {target: {value: 'johndoe@email.com'}});
         fireEvent.change(inputPassword, {target: {value: 'Pass@01'}});
@@ -215,21 +143,13 @@ describe("Login", () => {
         expect(inputEmail.value).toEqual('johndoe@email.com');
 
         await waitFor(() => {
-            void userEvent.click(submitButton);
+            void userEvent.click(loginButton);
             expect(getByText(/password must be at least 8 characters/i)).toBeInTheDocument();
         })
     })
 
     test("Should send maximum characters length error message", async () => {
-        const {getByText, getByLabelText} = render(
-            <BrowserRouter>
-                <Login/>
-            </BrowserRouter>
-        );
-
-        const inputEmail = getByLabelText('Email') as HTMLInputElement;
-        const inputPassword = getByLabelText('Password') as HTMLInputElement;
-        const submitButton = getByText('Log In!') as HTMLButtonElement;
+        const {getByText, inputEmail, inputPassword, loginButton} = createTestVariables();
 
         fireEvent.change(inputEmail, {target: {value: 'johndoe@email.com'}});
         fireEvent.change(inputPassword, {target: {value: 'Password@01Password@01Password@01Password@01Password@01Password@0'}});
@@ -237,7 +157,7 @@ describe("Login", () => {
         expect(inputEmail.value).toEqual('johndoe@email.com');
 
         await waitFor(() => {
-            void userEvent.click(submitButton);
+            void userEvent.click(loginButton);
             expect(getByText(/password must be at most 64 characters/i)).toBeInTheDocument();
         })
     })
