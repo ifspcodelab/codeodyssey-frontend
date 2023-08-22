7f8651aecbca9ba686735308b3ee66ee3c210c50
rafaelspa <rsdepaula67@gmail.com>
2023-08-22 15:37:47 -0300

    feat: add spinner, messages

--- src/pages/Confirmation/index.tsx
+++ src/pages/Confirmation/index.tsx
@@ -1,27 +1,35 @@
 import PageHeader from "../../components/PageHeader";
 import PageFooter from "../../components/PageFooter";
 import {useParams} from "react-router-dom"
-import {Container} from "@mui/material";
+import {Container, Typography} from "@mui/material";
 import {t} from "i18next";
 import {useEffect, useState} from "react";
 import {useApiRegistration} from "../../core/hooks/useApiRegistration";
 import {AxiosError} from "axios";
+import Spinner from "../../components/Spinner";
 
 const Confirmation = () => {
     const { token } = useParams();
-    const api = useApiRegistration();
-    const [confirmationError, setConfirmationError] = useState<string | null>(null)
-    const [confirmationMessage, setConfirmationMessage] = useState<string | null>(null)
+    const { confirmation } = useApiRegistration();
+    const [confirmationError, setConfirmationError] = useState<string | null>(null);
+    const [email, setEmail] = useState<string | null>(null);
+    const [loading, setLoading] = useState(false);
+    const [requested, setRequested] = useState(false);
 
     useEffect(() => {
-        api.confirmation(token)
+        if (!requested) {
+        setLoading(true);
+        confirmation(token)
             .then((response) => {
-                console.log(response);
-                setConfirmationMessage(response)
+                setEmail(response.email)
+                setLoading(false);
             }).catch((error: AxiosError) => {
                 handleError(error);
+                setLoading(false);
             });
-    }, []);
+        }
+        setRequested(true);
+    }, [requested, confirmation, token]);
 
     function handleError(error: AxiosError) {
         let responseStatus: number
@@ -30,29 +38,31 @@ const Confirmation = () => {
             problemDetail = error.response.data as ProblemDetail
             responseStatus = problemDetail.status
             if (responseStatus == 400) {
-                console.log('bad request')
-                setConfirmationError('bad request')
+                setConfirmationError('badRequest')
             } else if (responseStatus == 409) {
                 if (error.response) problemDetail = error.response.data as ProblemDetail
                 if (problemDetail.title == "Validation" && problemDetail.detail == "User is already validated")
-                    console.log('email already exists')
-                    setConfirmationError('email already exists')
+                    setConfirmationError('exists')
+            } else if (responseStatus == 404) {
+                if (error.response) problemDetail = error.response.data as ProblemDetail
+                if (problemDetail.title == "Token problem" && problemDetail.detail == "No user associated with this token")
+                    setConfirmationError('notFound')
             }
         } else if (error.message == "Network Error") {
-            console.log('network error')
-            setConfirmationError('network error')
+            setConfirmationError('network')
         }
     }
 
     return (
-        <div>
-            <Container maxWidth="md">
-                <PageHeader title={t('confirmation.title')} text={t('confirmation.text')}/>
-                {confirmationMessage && <p className="error">{confirmationMessage}</p>}
-                {confirmationError && <p className="error">{confirmationError}</p>}
-                <PageFooter text={t('confirmation.footer')}/>
-            </Container>
-        </div>
+        <Container maxWidth="md">
+            <PageHeader title={t('confirmation.title')} text={t('confirmation.text')}/>
+            { loading ? <Spinner size={40}/> :
+                <Typography variant="h4" component="div">
+                    { email && <p>{t('confirmation.success', {email: email})}</p> }
+                    { confirmationError && <p>{t('confirmation.error.' + confirmationError)}</p> }
+                </Typography> }
+            <PageFooter text={t('confirmation.footer')}/>
+        </Container>
     );
 };
 
