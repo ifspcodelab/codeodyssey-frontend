5e0e0ff4fd30914b8581c161454c11631e81098b
rafaelspa <rsdepaula67@gmail.com>
2023-08-22 16:44:29 -0300

    test: add request mock to confirmation endpoint

--- src/pages/Confirmation/confirmation.test.tsx
+++ src/pages/Confirmation/confirmation.test.tsx
@@ -3,7 +3,34 @@ import {render} from "@testing-library/react";
 import {BrowserRouter} from "react-router-dom";
 import Confirmation from "./index";
 import "@testing-library/jest-dom";
+import {rest} from "msw";
+import {setupServer} from "msw/node";
 
+export const restHandlers = [
+    rest.patch('http://localhost:3000/users/confirmation/undefined', (req, res, ctx) => {
+        console.log(req) // for build purposes
+        return res(
+            ctx.status(200),
+            ctx.json(
+                {
+                    "id": "b16107b2-a021-4da5-b4f5-bc3cd9e8c759",
+                    "name": "John Doe",
+                    "email": "john@doe.com",
+                    "role": "STUDENT",
+                    "createdAt": "2023-08-22T19:07:21.170037Z"
+                }
+            )
+        )
+    }),
+];
+
+const server = setupServer(...restHandlers)
+
+beforeAll(() => void server.listen({ onUnhandledRequest: 'error' }))
+
+afterAll(() => void server.close())
+
+afterEach(() => server.resetHandlers())
 
 function renderConfirmation() {
     return render(
