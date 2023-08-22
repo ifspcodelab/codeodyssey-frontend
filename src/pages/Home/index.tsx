9bfbaab6775e7ac6e76c94d2d8db2c0c9848131c
vinicius <viniciusarashiro0@gmail.com>
2023-08-22 14:57:32 -0300

    refactor: restyle home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,45 @@
-import PageHeader from "../../components/PageHeader";
-import PageFooter from "../../components/PageFooter";
-import {Link} from "react-router-dom";
+import {useNavigate} from "react-router-dom"
+import {
+    AppBar,
+    Tab,
+    Tabs,
+    Toolbar,
+    Typography
+  } from "@mui/material";
+
 
 function Home() {
+  const navigate = useNavigate()
+
     return (
         <>
-            <PageHeader title={'Home'} text={"Application's home"}/>
-            <h1>Home Page</h1>
-            <nav>
-                <Link to={"/registration"}>Registration</Link>
-                <Link to={"/login"}>Login</Link>
-                <Link to={"/create-course"}>Create course</Link>
-            </nav>
-            <PageFooter text={"Home Footer"}/>
+            <AppBar sx={{ background: "#063970" }}>
+                <Toolbar>
+                <Typography sx={{  paddingLeft: "10%", cursor: "pointer" }} onClick={() => {
+                            navigate('/')
+                            }}>
+                    CodeOdyssey
+                </Typography>
+                <Tabs sx={{ marginLeft: "auto" }}
+                >
+                    <Tab sx={{color: "#fff"}} onClick={() => {
+                            navigate('/registration')
+                            }}label="Register" />
+                    <Tab sx={{color: "#fff"}} onClick={() => {
+                            navigate('/login')
+                            }}label="Login" />
+                    <Tab sx={{color: "#fff"}} onClick={() => {
+                            navigate('/courses')
+                            }}label="My Courses" />
+                    <Tab sx={{color: "#fff"}} onClick={() => {
+                            navigate('/create-course')
+                            }}label="Create Course" />
+                    <Tab sx={{color: "#fff"}} onClick={() => {
+                            navigate('/contact')
+                            }}label="Contact" />
+                </Tabs>
+                </Toolbar>
+            </AppBar>
         </>
     );
 }
