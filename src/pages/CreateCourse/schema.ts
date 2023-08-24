a1db1ce8699c5c449e776b51aff19193e72c9871
vinicius <viniciusarashiro0@gmail.com>
2023-08-24 15:03:39 -0300

    fix: create course validation messages

--- src/pages/CreateCourse/schema.ts
+++ src/pages/CreateCourse/schema.ts
@@ -1,9 +1,11 @@
 import Yup from "../../config/yup.ts";
+import i18n from "../../locales/i18n";
+
 
 export const schema = Yup.object({
     name: Yup.string().required().min(1).max(255),
     slug: Yup.string().required().min(1).max(255).matches(
-        /^[a-zA-Z0-9-]+$/, "This field cannot contain white space and special character"),
+        /^[a-zA-Z0-9-]+$/,  i18n.t('createcourse.form.validation.special')),
     startDate: Yup.date().required(),
-    endDate: Yup.date().required().min(Yup.ref('startDate'), "The end date cannot be earlier than start date")
+    endDate: Yup.date().required().min(Yup.ref('startDate'), i18n.t('createcourse.form.validation.endDate'))
 }).required()
