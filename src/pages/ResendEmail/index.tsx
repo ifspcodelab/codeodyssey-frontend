5c9593400e59578023352856339dfc28e6334d60
rafaelspa <rsdepaula67@gmail.com>
2023-08-25 14:38:55 -0300

    feat: add resend email component

--- src/pages/ResendEmail/index.tsx
+++ src/pages/ResendEmail/index.tsx
@@ -1,14 +1,42 @@
 import PageHeader from "../../components/PageHeader";
-import {useTranslation} from "react-i18next";
+import {Trans, useTranslation} from "react-i18next";
 import {useLocation} from "react-router-dom"
+import PageFooter from "../../components/PageFooter";
+import {Container, Link, Typography} from "@mui/material";
+import {useApiRegistration} from "../../core/hooks/useApiRegistration";
+
+
+interface UseLocationState {
+    data: string;
+}
 
 function ResendEmail() {
-    const {t} = useTranslation()
-    const location = useLocation()
+    const {t} = useTranslation();
+    const location = useLocation();
+    const { resendConfirmationEmail } = useApiRegistration();
+
+    function resendEmail() {
+        const state = location.state as UseLocationState;
+        resendConfirmationEmail(state.data)
+            .then((response) => {
+                console.log(response);})
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
 
     return (
-        <PageHeader title={t("resendEmail.title")} text={t("resendEmail.text", { email: location.state.data })}/>
-    );
+        <Container maxWidth="md">
+            <PageHeader title={t("resendEmail.title")} text={t("resendEmail.text", { email: location.state.data })}/>
+            <Typography variant="h6" gutterBottom>{t("resendEmail.emailMessage", { email: location.state.data })}</Typography>
+            <Trans i18nKey="resendEmail.linkMessage">
+                Click <Link sx={{marginRight: '3px'}} component="button" variant="body3" onClick={resendEmail}>here</Link>
+                to resend the email.
+            </Trans>
+            <PageFooter text={t('resendEmail.footer')}/>
+        </Container>
+    )
 }
 
 export default ResendEmail;
