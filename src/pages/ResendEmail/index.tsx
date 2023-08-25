646519a626e8d163207b803ba51d71ce53afeff0
rafaelspa <rsdepaula67@gmail.com>
2023-08-25 15:55:27 -0300

    feat: add error handler to resend email

--- src/pages/ResendEmail/index.tsx
+++ src/pages/ResendEmail/index.tsx
@@ -4,6 +4,10 @@ import {useLocation} from "react-router-dom"
 import PageFooter from "../../components/PageFooter";
 import {Container, Link, Typography} from "@mui/material";
 import {useApiRegistration} from "../../core/hooks/useApiRegistration";
+import {useState} from "react";
+import Spinner from "../../components/Spinner";
+import ErrorSnackBar from "../../components/ErrorSnackBar/ErrorSnackBar";
+import {AxiosError} from "axios";
 
 
 interface UseLocationState {
@@ -14,26 +18,68 @@ function ResendEmail() {
     const {t} = useTranslation();
     const location = useLocation();
     const { resendConfirmationEmail } = useApiRegistration();
+    const [loading, setLoading] = useState(false);
+    const [resendSuccess, setResendSuccess] = useState(false);
+    const [open, setOpen] = useState(false);
+    const [errorType, setErrorType] = useState('');
 
-    function resendEmail() {
+    const resendEmail = () => {
+        setLoading(true);
         const state = location.state as UseLocationState;
         resendConfirmationEmail(state.data)
-            .then((response) => {
-                console.log(response);})
-            .catch((error) => {
-                console.log(error);
+            .then(() => {
+                setLoading(false);
+                setResendSuccess(true);
+            }).catch((error) => {
+                setLoading(false);
+                handleError(error);
             })
     }
 
+    const handleError = (error: AxiosError) => {
+        let responseStatus: number;
+        let problemDetail: ProblemDetail = { title: '', detail: '' , instance: '', status: 0, type: ''};
+        if (error.response) {
+            problemDetail = error.response?.data as ProblemDetail;
+            responseStatus = problemDetail.status;
+            if (responseStatus === 404) {
+                setErrorType('notFound');
+                setOpen(true);
+            } else if (responseStatus === 422) {
+                let problemDetail = error.response?.data as ProblemDetail;
+                if (problemDetail.title == "Time to resend not passed" && problemDetail.detail.startsWith("One minute to resend email to"))
+                    setErrorType('resendEmailDelay')
+                setOpen(true);
+            }
+        } else if (error.message == "Network Error") {
+            setErrorType('networkError');
+            setOpen(true);
+        }
+    }
+
+    const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway' || event === undefined) {
+            return;
+        }
+
+        setOpen(false);
+    };
 
     return (
         <Container maxWidth="md">
             <PageHeader title={t("resendEmail.title")} text={t("resendEmail.text", { email: location.state.data })}/>
             <Typography variant="h6" gutterBottom>{t("resendEmail.emailMessage", { email: location.state.data })}</Typography>
-            <Trans i18nKey="resendEmail.linkMessage">
-                Click <Link sx={{marginRight: '3px'}} component="button" variant="body3" onClick={resendEmail}>here</Link>
-                to resend the email.
-            </Trans>
+            {!resendSuccess ?
+                <Trans i18nKey="resendEmail.linkMessage">
+                    Click <Link sx={{marginRight: '3px'}} component="button" variant="body3"
+                                onClick={resendEmail}>here</Link>
+                    to resend the email.
+                </Trans>
+                :
+                <Typography variant="subtitle" gutterBottom>{t("resendEmail.successMessage")}</Typography>
+            }
+            {loading && <Spinner size={10}/>}
+            <ErrorSnackBar open={open} handleClose={handleClose} errorType={errorType}/>
             <PageFooter text={t('resendEmail.footer')}/>
         </Container>
     )
