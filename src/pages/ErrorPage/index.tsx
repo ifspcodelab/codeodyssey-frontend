388004b5cf9cd64edf6aa1a87cd24f748d897ea2
rafaelspa <rsdepaula67@gmail.com>
2023-08-28 15:06:13 -0300

    refactor: error page component to admit no email in resend email page

--- src/pages/ErrorPage/index.tsx
+++ src/pages/ErrorPage/index.tsx
@@ -9,16 +9,21 @@ interface ErrorResponse {
     error: string;
 }
 
-export default function ErrorPage() {
-    const error = useRouteError() as ErrorResponse;
+interface StatusType {
+    status?: number;
+}
+
+export default function ErrorPage({status}: ErrorResponse | StatusType) {
+    const routeError = useRouteError() as ErrorResponse;
     const { t } = useTranslation();
 
     return (
         <div id="error-page">
-            <h1>{t("errorPage.title", {statusCode: error.status})}</h1>
+            <h1>{t("errorPage.title", {statusCode: routeError?.status || status})}</h1>
             <p>{t("errorPage.subTitle")}</p>
             <p>
-                <i>{error.status === 404 && t("errorPage.notFound.message")}</i>
+                <i>{(routeError?.status == 404 || status == 404) ? t("errorPage.message.notFound") : ""}</i>
+                <i>{(routeError?.status == 400 || status == 400) ? t("errorPage.message.badRequest") : ""}</i>
             </p>
         </div>
     );
