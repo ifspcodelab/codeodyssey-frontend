1446e2d9bca1cd5899b33791b82948859c0b3e3e
rafaelspa <rsdepaula67@gmail.com>
2023-08-29 15:57:32 -0300

    test: should show successfull message when user clicks on the resend button

--- src/pages/ResendEmail/resend-email.test.tsx
+++ src/pages/ResendEmail/resend-email.test.tsx
@@ -1,8 +1,10 @@
 import {describe, test, vi} from "vitest";
-import {render} from "@testing-library/react";
+import {render, waitFor, fireEvent} from "@testing-library/react";
 import ResendEmail from "./index";
 import {BrowserRouter} from "react-router-dom";
 import "@testing-library/jest-dom";
+import {rest} from "msw";
+import {setupServer} from "msw/node";
 
 interface UseLocationType {
     state: {
@@ -29,6 +31,32 @@ function renderResendEmail() {
     );
 }
 
+export const restHandlers = [
+    rest.post('http://localhost:3000/users/resend-email', (req, res, ctx) => {
+        console.log(req) // for build purposes
+        return res(
+            ctx.status(200),
+            ctx.json(
+                {
+                    "id": "c29a994b-4423-499f-b09e-f3c92679b974",
+                    "name": "John Doe",
+                    "email": "johndoe@email.com",
+                    "role": "STUDENT",
+                    "createdAt": "2023-08-29T18:41:42.042243Z"
+                }
+            )
+        )
+    }),
+];
+
+const server = setupServer(...restHandlers)
+
+beforeAll(() => void server.listen({ onUnhandledRequest: 'error' }))
+
+afterAll(() => void server.close())
+
+afterEach(() => server.resetHandlers())
+
 describe("Resend Email", () => {
     test("Should be able to see page elements on the screen", () => {
         const {getByRole} = renderResendEmail();
@@ -37,5 +65,17 @@ describe("Resend Email", () => {
         expect(getByRole("heading", { name: "In case you didn't receive the email, you can resend it by clicking the link below after one minute has passed."})).toBeInTheDocument();
         expect(getByRole("heading", { name: "The system has sent a confirmation email to johndoe@email.com. Check your email."})).toBeInTheDocument();
         expect(getByRole("button", { name: "here"})).toBeInTheDocument();
+    });
+
+    test("Should show successfull message when user clicks on the resend button", async () => {
+        const {getByRole, getByText} = renderResendEmail();
+
+        const resendButton = getByRole("button", { name: "here"});
+
+        fireEvent.click(resendButton);
+
+        await waitFor(() => {
+            expect(getByText("The email was resent successfully.")).toBeInTheDocument();
+        });
     });
 });
