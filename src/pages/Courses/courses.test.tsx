9b98891c9749bbb759c6bf19f90f068beb664778
vinicius <viniciusarashiro0@gmail.com>
2023-08-31 15:47:26 -0300

    test: testing the buttons on the page courses

--- src/pages/Courses/courses.test.tsx
+++ src/pages/Courses/courses.test.tsx
@@ -1,76 +1,202 @@
-import { render } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import { describe, test, vi } from "vitest";
-import { BrowserRouter } from "react-router-dom";
-import Courses from "./index";
+import React from 'react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
+import Courses from '../Courses';
+import { useApiGetCourses } from '../../core/hooks/useApiGetCourses.ts';
+import { AuthContext } from '../../core/auth/AuthContext.tsx';
+import { vi } from 'vitest';
+import { BrowserRouter, Router } from 'react-router-dom';
+import '@testing-library/jest-dom';
+import { UserRole } from '../../core/auth/JwtService.ts';
+import { createMemoryHistory } from "history";
 
-describe("Courses", () => {
-  beforeEach(() => {
-    vi.clearAllMocks();
-  });
+vi.mock('../../core/hooks/useApiGetCourses.ts');
 
-  vi.mock('../../core/hooks/useApiGetCourses', () => ({
-    useApiGetCourses: () => ({
-      getCoursesStudent: () => [
-        {
-          id: 1, name: 'Course 1', professor: { name: 'Professor A' }, startDate: '2023-08-15',
-          endDate: '2023-08-30',
-        },
-        {
-          id: 2, name: 'Course 2', professor: { name: 'Professor B' }, startDate: '2023-08-18',
-          endDate: '2023-11-30',
-        },
-      ],
-      getCoursesProfessor: () => [
-        {
-          id: 3, name: 'Course 3', professor: { name: 'Professor C' }, startDate: '2023-08-16',
-          endDate: '2023-09-30',
-        },
-        {
-          id: 4, name: 'Course 4', professor: { name: 'Professor D' }, startDate: '2023-08-17',
-          endDate: '2023-10-30',
-        },
-      ],
-    }),
-  }));
+const mockGetCoursesStudent = vi.fn();
+const mockGetCoursesProfessor = vi.fn();
 
+(useApiGetCourses as jest.Mock).mockReturnValue({
+  getCoursesStudent: mockGetCoursesStudent,
+  getCoursesProfessor: mockGetCoursesProfessor,
+});
+
+interface MockAuthProviderProps {
+  children: React.ReactNode;
+  role: UserRole;
+}
+
+const MockAuthProvider: React.FC<MockAuthProviderProps> = ({ children, role }) => (
+  <AuthContext.Provider
+    value={{
+      authenticated: true,
+      setAuthenticated: vi.fn(),
+      accessToken: 'test-token',
+      setAccessToken: vi.fn(),
+      id: '1',
+      setId: vi.fn(),
+      email: 'test@example.com',
+      setEmail: vi.fn(),
+      roles: role,
+      setRole: vi.fn(),
+    }}
+  >
+    {children}
+  </AuthContext.Provider>
+);
+
+describe("Visualize my courses", () => {
   test("Should be able to see the Page Header title on the screen", () => {
     const { getByText } = render(
-        <BrowserRouter>
-            <Courses/>
-        </BrowserRouter>
-    );
-
-    expect(getByText("My Courses")).toBeInTheDocument();
-  })  
-
-  test('should render course attributes correctly', async () => {
-    const { findByText } = render(
-      <BrowserRouter>
-        <Courses/>
-      </BrowserRouter>);
-
-    const courseName = await findByText(/Course 1/i);
-    const professorName = await findByText(/Professor A/i);
-    const startDate = await findByText(/8\/15\/2023/i);
-    const endDate = await findByText(/8\/30\/2023/i);
-
-    expect(courseName).toBeInTheDocument();
-    expect(professorName).toBeInTheDocument();
-    expect(startDate).toBeInTheDocument();
-    expect(endDate).toBeInTheDocument();
-  });
-
-  test("should render all student courses", async () => {
-    const { findAllByText } = render(
       <BrowserRouter>
         <Courses />
       </BrowserRouter>
     );
-    const courseNames = await findAllByText(/Course \d/i);
 
-    expect(courseNames).toHaveLength(2);
+    expect(getByText("My Courses")).toBeInTheDocument();
   })
-});
 
+  test('Should be able to render courses professor', async () => {
+    const mockCourse = {
+      id: 1,
+      name: 'Java Spring Course',
+      professor: { name: 'John Doe' },
+      startDate: '8/30/2023',
+      endDate: '9/30/2023',
+    };
 
+    mockGetCoursesProfessor.mockResolvedValue([mockCourse]);
+
+    const { getByText } = render(
+      <BrowserRouter>
+        <MockAuthProvider role={UserRole.PROFESSOR}>
+          <Courses />
+        </MockAuthProvider>
+      </BrowserRouter>
+    );
+
+    const element = await waitFor(() => getByText('Java Spring Course'));
+    expect(element).toBeInTheDocument();
+
+    const professorName = await waitFor(() => getByText('John Doe'));
+    expect(professorName).toBeInTheDocument();
+
+    const startDate = await waitFor(() => getByText(/8\/30\/2023/i));
+    expect(startDate).toBeInTheDocument();
+
+    const endDate = await waitFor(() => getByText(/9\/30\/2023/i));
+    expect(endDate).toBeInTheDocument();
+  });
+
+  test('Should be able to render courses student', async () => {
+    const mockCourse = {
+      id: 1,
+      name: 'React Course',
+      professor: { name: 'Moriarty' },
+      startDate: '8/30/2023',
+      endDate: '9/30/2023',
+    };
+
+    mockGetCoursesStudent.mockResolvedValue([mockCourse]);
+
+    const { getByText } = render(
+      <BrowserRouter>
+        <MockAuthProvider role={UserRole.STUDENT}>
+          <Courses />
+        </MockAuthProvider>
+      </BrowserRouter>
+    );
+
+    const courseName = await waitFor(() => getByText('React Course'));
+    expect(courseName).toBeInTheDocument();
+
+    const professorName = await waitFor(() => getByText('Moriarty'));
+    expect(professorName).toBeInTheDocument();
+
+    const startDate = await waitFor(() => getByText(/8\/30\/2023/i));
+    expect(startDate).toBeInTheDocument();
+
+    const endDate = await waitFor(() => getByText(/9\/30\/2023/i));
+    expect(endDate).toBeInTheDocument();
+  });
+
+  test('Should be able to render courses professor button', async () => {
+    const mockCourse = {
+      id: 1,
+      name: 'React Course',
+      professor: { name: 'Moriarty' },
+      startDate: new Date().toISOString(),
+      endDate: new Date().toISOString(),
+    };
+
+    mockGetCoursesProfessor.mockResolvedValue([mockCourse]);
+
+    const { getByText } = render(
+      <BrowserRouter>
+        <MockAuthProvider role={UserRole.PROFESSOR}>
+          <Courses />
+        </MockAuthProvider>
+      </BrowserRouter>
+    );
+
+    const createInviteButton = await waitFor(() => getByText('Create Invite'));
+    const visualizeStudentsButton = await waitFor(() => getByText('Visualize Students'));
+    expect(createInviteButton).toBeInTheDocument();
+    expect(visualizeStudentsButton).toBeInTheDocument();
+  });
+
+  test('Should be able to render empty list', async () => {
+    mockGetCoursesStudent.mockResolvedValue([]);
+
+    const { getByText } = render(
+      <BrowserRouter>
+        <MockAuthProvider role={UserRole.STUDENT}>
+          <Courses />
+        </MockAuthProvider>
+      </BrowserRouter>
+    );
+
+    const courseName = await waitFor(() => getByText('You do not participate in any course yet'));
+    expect(courseName).toBeInTheDocument();
+  });
+
+  test('Should be able to navigate to another pages after click buttons', async () => {
+    const history = createMemoryHistory();
+    history.push = vi.fn();
+
+    mockGetCoursesStudent.mockResolvedValue([]);
+    const { getByText } = render(
+
+      <Router location={history.location} navigator={history}>
+        <MockAuthProvider role={UserRole.PROFESSOR}>
+          <Courses />
+        </MockAuthProvider>
+      </Router>
+    );
+
+    const visualizeStudentsButton = await waitFor(() => getByText('Visualize Students'));
+    const createInviteButton = await waitFor(() => getByText('Create Invite'));
+
+    await waitFor(() => {
+      fireEvent.click(visualizeStudentsButton)
+      expect(history.push).toHaveBeenLastCalledWith({
+        "hash": "",
+        "pathname": "/students",
+        "search": "",
+      },
+        undefined,
+        {},
+      );
+    });
+
+    await waitFor(() => {
+      fireEvent.click(createInviteButton)
+      expect(history.push).toHaveBeenLastCalledWith({
+        "hash": "",
+        "pathname": "/invitation",
+        "search": "",
+      },
+        undefined,
+        {},
+      );
+    });
+  });
+})
