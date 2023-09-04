593491117ad5050965269b8b0c91b66345a7d846
vinicius <viniciusarashiro0@gmail.com>
2023-09-04 16:04:02 -0300

    feat: translate page

--- src/pages/Students/index.tsx
+++ src/pages/Students/index.tsx
@@ -1,4 +1,4 @@
-import { useApiGetStudents} from "../../core/hooks/useApiGetStudents.ts";
+import { useApiGetStudents } from "../../core/hooks/useApiGetStudents.ts";
 import { useEffect, useState } from "react";
 import { AuthConsumer } from "../../core/auth/AuthContext.tsx";
 import Card from '@mui/material/Card';
@@ -7,6 +7,8 @@ import Typography from '@mui/material/Typography';
 import { JwtService } from "../../core/auth/JwtService.ts";
 import { useParams } from "react-router-dom";
 import { StudentResponse } from "../../core/models/StudentResponse";
+import { useTranslation } from "react-i18next";
+import PageHeader from "../../components/PageHeader";
 
 function Students() {
   const [students, setStudents] = useState<StudentResponse[] | ProblemDetail>([]);
@@ -14,47 +16,47 @@ function Students() {
   const authConsumer = AuthConsumer();
   const rawAccessToken = new JwtService().getRawAccessToken() as string;
   const { slug } = useParams()
-  
+  const { t } = useTranslation();
   const USER_ID: string = authConsumer.id;
   useEffect(() => {
     void (async () => {
-      if(typeof slug === 'string') {
+      if (typeof slug === 'string') {
         try {
           console.log(typeof slug)
           const studentsResponse = await getStudents(USER_ID, slug, rawAccessToken)
           setStudents(studentsResponse)
           console.log("id: ", slug)
-          console.log("teste" , studentsResponse)
+          console.log("teste", studentsResponse)
         } catch (error) {
           console.log(error)
-      }
+        }
       } else {
         console.log("error")
       }
-        
+
     })();
     // eslint-disable-next-line
   }, [rawAccessToken]);
 
 
   return (
-      <>
-          <h1>Students</h1>
-          <div>
-          {Array.isArray(students) && students.map((student: StudentResponse) => (
-                <Card key={student.id} variant="outlined" className="cardContainer">
-                  <CardContent>
-                    <Typography variant="h5" component="div">
-                      Name: {student.name}
-                    </Typography>
-                    <Typography variant="h5" component="div">
-                      Email: {student.email}
-                    </Typography>
-                  </CardContent>               
-                </Card>
-              ))}
-          </div>
-      </>
+    <>
+      <PageHeader title={t('students.title')} text={t('students.text')} />
+      <div>
+        {Array.isArray(students) && students.map((student: StudentResponse) => (
+          <Card key={student.id} variant="outlined" className="cardContainer">
+            <CardContent>
+              <Typography variant="h5" component="div">
+                {t('students.name')}: {student.name}
+              </Typography>
+              <Typography variant="h5" component="div">
+                {t('students.email')}: {student.email}
+              </Typography>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </>
   );
 }
 
