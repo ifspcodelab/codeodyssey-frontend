9ace7d347f33475ef8939e937bf848caec4cc6da
rafaelspa <rsdepaula67@gmail.com>
2023-09-12 16:11:28 -0300

    feat: add hooks, error messages, spinner in invitation component

--- src/pages/Invitation/index.tsx
+++ src/pages/Invitation/index.tsx
@@ -1,27 +1,66 @@
 import {useParams} from "react-router-dom";
-import {useEffect} from "react";
+import {useCallback, useEffect, useRef, useState} from "react";
 import {useApiAcceptInvitation} from "../../core/hooks/useApiAcceptInvitation";
-import { JwtService } from "../../core/auth/JwtService";
+import PageHeader from "../../components/PageHeader";
+import {useTranslation} from "react-i18next";
+import PageFooter from "../../components/PageFooter";
+import {AxiosError} from "axios";
+import {EnrollmentResponse} from "../../core/models/invitations";
+import {Container, Typography} from "@mui/material";
+import Spinner from "../../components/Spinner";
+
 
 function Invitation() {
     const { idInvitation } = useParams();
     const { acceptInvitation } = useApiAcceptInvitation();
+    const { t } = useTranslation();
+    const [invitationText, setInvitationText] = useState("");
+    const requestSentRef = useRef(false);
+    const [loading, setLoading] = useState(false);
+
+    const handleErrors = useCallback((error: AxiosError) => {
+        switch (error.response?.status) {
+            case 400:
+                setInvitationText(t("invitation.errors.invalid"));
+                break;
+            case 403:
+                setInvitationText(t("invitation.errors.unauthorized"));
+                break;
+            case 404:
+                setInvitationText(t("invitation.errors.notFound"));
+                break;
+            case 409:
+                setInvitationText(t("invitation.errors.alreadyAccepted"));
+                break;
+            default:
+                setInvitationText(t("invitation.errors.network"));
+        }
+    }, [t]);
 
     useEffect(() => {
-        const jwtService = new JwtService();
-        const rawAccessToken = jwtService.getAccessToken();
-        acceptInvitation(idInvitation, rawAccessToken)
-            .then(() => {
-                console.log("Invitation accepted");
-            }).catch(() => {
-                console.log("Invitation not accepted");
-            })
-    }, [acceptInvitation, idInvitation]);
+        if (!requestSentRef.current) {
+            setLoading(true);
+            acceptInvitation(idInvitation)
+                .then((enrollmentResponse: EnrollmentResponse) => {
+                    setInvitationText(t("invitation.accepted", {courseName: enrollmentResponse.invitation.course.name }));
+                    setLoading(false);
+                }).catch((error: AxiosError) => {
+                handleErrors(error);
+                setLoading(false);
+            });
+        }
+        requestSentRef.current = true;
+    }, [acceptInvitation, handleErrors, idInvitation, t])
 
     return (
-      <>
-          <h1>Invitation</h1>
-      </>
+    <Container maxWidth="sm">
+        <PageHeader title={t("invitation.title")} text={t("invitation.text")}/>
+        <Typography align="center" paragraph>
+            {invitationText}
+        </Typography>
+        {loading && <Spinner size={20}/>}
+        <PageFooter text={t("invitation.footer")}/>
+    </Container>
     );
 }
 
