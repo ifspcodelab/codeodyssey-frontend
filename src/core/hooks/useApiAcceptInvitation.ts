45ff5093a4a350108ce816d394f501bed6c4b9ba
rafaelspa <rsdepaula67@gmail.com>
2023-09-12 16:05:34 -0300

    fix: change raw access token input origin

--- src/core/hooks/useApiAcceptInvitation.ts
+++ src/core/hooks/useApiAcceptInvitation.ts
@@ -1,11 +1,13 @@
 import {api} from "../services/axios";
-import {AccessToken} from "../auth/JwtService";
+import {JwtService} from "../auth/JwtService";
+
+const jwtService = new JwtService();
 
 export const useApiAcceptInvitation = () => ({
-    acceptInvitation: async (invitationId: string | undefined, rawAccessToken: AccessToken | null) => {
-        api.defaults.headers['Authorization'] = 'Bearer ' + rawAccessToken;
+    acceptInvitation: async (invitationId: string) => {
+        const raw = jwtService.getRawAccessToken() as string;
+        api.defaults.headers['Authorization'] = 'Bearer ' + raw;
         const response = await api.post< | ProblemDetail>(`/invitations/${invitationId}/enrollments`);
-
         return response.data;
     }
 })
