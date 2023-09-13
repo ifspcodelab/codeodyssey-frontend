b72f3fa23aaa8e48c5320362c6dce5e9866ea3c1
vinicius <viniciusarashiro0@gmail.com>
2023-09-13 16:52:48 -0300

    fix: build erros in students page

--- src/core/hooks/useApiGetStudents.ts
+++ src/core/hooks/useApiGetStudents.ts
@@ -1,4 +1,3 @@
-import { StudentResponse } from "../models/StudentResponse";
 import {api} from "../services/axios";
 
 
@@ -7,7 +6,7 @@ export const useApiGetStudents = () => ({
     getStudents: async (USER_ID: string, courseSlug: string, rawAccessToken: string) => {
         console.log("rawAccessToken: ", rawAccessToken);
         api.defaults.headers['Authorization'] = 'Bearer ' + rawAccessToken;
-        const response = await api.get<StudentResponse | ProblemDetail>('/users/' + USER_ID + '/courses/' + courseSlug + '/students');
+        const response = await api.get< | ProblemDetail>('/users/' + USER_ID + '/courses/' + courseSlug + '/students');
         return response.data;
     },
 })
